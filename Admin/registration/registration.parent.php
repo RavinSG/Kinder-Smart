5b73c83214e52d277d4d5076a6091cfcb6145145
Harith Sankalpa <harith.sankalpa@gmail.com>
2018-07-06 18:45:26 +0530

    add form validation for registering

--- Admin/registration/registration.parent.php
+++ Admin/registration/registration.parent.php
@@ -31,20 +31,19 @@
 				<option name="salutation" value="Mrs."> Mrs. </option>
 			</select>
 			<br />
-			<label>Full Name :</label> <input type="text" name="full_name" value=<?php if(isset($_SESSION['parent_full_name'])){echo "{$_SESSION['parent_full_name']}";} else{echo "";}?> > 
-			<br />
-			<label>Name with Initials:</label><input type="text" name="ini_name" value=<?php if(isset($_SESSION['parent_ini_name'])){echo "{$_SESSION['parent_ini_name']}";} else{echo "";}?> > 
-			<br />
-			<label>NIC number :</label> <input type="text" name="nic" size="10" maxlength="10" value=<?php if(isset($_SESSION['nic'])){echo "{$_SESSION['nic']}";} else{echo "";}?> > 
-			<br />
-			<label>Address :</label> <input type="text" name="address" value= <?php if(isset($_SESSION['parent_address'])){echo "{$_SESSION['parent_address']}";} else{echo "";}?>> 
-			<br />
-			<label>Email :</label> <input type="text" name="email" value=<?php if(isset($_SESSION['email'])){echo "{$_SESSION['email']}";} else{echo "";}?>> 
-			<br />
-			<label>Telephone Number :</label> <input type="text" name="tele_no" value=<?php if(isset($_SESSION['tele_no'])){echo "{$_SESSION['tele_no']}";} else{echo "";}?>> 
-			<label>Mobile Number :</label> <input type="text" name="mobile_no" value=<?php if(isset($_SESSION['mobile_no'])){echo "{$_SESSION['mobile_no']}";} else{echo "";}?>> 
-			<br />
-			<label>Occupation :</label> <input type="text" name="occupation" value=<?php if(isset($_SESSION['occupation'])){echo "{$_SESSION['occupation']}";} else{echo "";}?>> 
+            <label>Full Name :</label> <input type="text" name="full_name" required value=<?php if(isset($_SESSION['admin_full_name'])){echo "{$_SESSION['admin_full_name']}";} else{echo "";}?> >
+            <br />
+            <label>Name with Initials:</label><input type="text" name="ini_name" required value=<?php if(isset($_SESSION['admin_ini_name'])){echo "{$_SESSION['admin_ini_name']}";} else{echo "";}?>>
+            <br />
+            <label>NIC number :</label> <input type="text" name="nic" size="10" minlength="10" maxlength="10" pattern="[0-9]{9}[V|X]" required value=<?php if(isset($_SESSION['nic'])){echo "{$_SESSION['nic']}";} else{echo "";}?>  >
+            <br />
+            <label>Address :</label> <input type="text" name="address" required value= <?php if(isset($_SESSION['admin_address'])){echo "{$_SESSION['admin_address']}";} else{echo "";}?> >
+            <br />
+            <label>Email :</label> <input type="text" name="email" required pattern="[a-z0-9._%]+@[a-z0-9.]+\.[a-z]{2,3}$" value=<?php if(isset($_SESSION['email'])){echo "{$_SESSION['email']}";} else{echo "";}?> >
+            <br />
+            <label>Telephone Number :</label> <input type="text" name="tele_no" required  minlength="10" maxlength="10" value=<?php if(isset($_SESSION['tele_no'])){echo "{$_SESSION['tele_no']}";} else{echo "";}?>>
+            <label>Mobile Number :</label> <input type="text" name="mobile_no" minlength="10" maxlength="10" required value=<?php if(isset($_SESSION['mobile_no'])){echo "{$_SESSION['mobile_no']}";} else{echo "";}?> >
+			<label>Occupation :</label> <input type="text" name="occupation" required pattern="[a-zA-Z]+" title="Please enter only alphabetic characters" value=<?php if(isset($_SESSION['occupation'])){echo "{$_SESSION['occupation']}";} else{echo "";}?>>
 			<br />
 			<input type="submit" name="submit" value="Submit" />
 		</form>
