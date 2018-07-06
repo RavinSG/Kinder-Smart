1127972fdc4abc6e6e83d06f03d46ecd01c97971
Harith Sankalpa <harith.sankalpa@gmail.com>
2018-07-07 00:16:20 +0530

    added view message function to parent

---
+++ Parent/viewMessage.php
@@ -0,0 +1,36 @@
+<?php  
+	include '../include/conToParentMessage.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>view messages</title>
+	<link rel="stylesheet" href="include/table.css">
+</head>
+<body>
+    <?php
+    	if ($dates==NULL or $messages==NULL) {
+    		echo "No unread messages";
+    	} else {
+    		echo"<table cellspacing='2'>";
+    		echo"<tr>";
+    		echo "Date";
+    		echo "</tr>";
+    		echo "<tr>";
+    		echo "Message";
+    		echo "</tr>";
+    		for ($i = 0; $i < sizeof($dates); $i++)
+			{
+    			echo "<tr><td>";
+            	echo $dates[$i];
+            	echo "</td><td>";
+            	echo $messages[$i];
+            	echo "</td><td>";
+            	echo "<br>";
+			}
+			echo "</table>";
+		}
+	?>
+	
+</body>
+</html>
