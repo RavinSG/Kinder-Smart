fd1d3e689e20b006f8f63b9aaf3884b9fa7bc6e0
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-07-07 10:41:34 +0530

    Completed the marking attendance function.

--- Parent/checklogin.parent.php
+++ Parent/checklogin.parent.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Harith
- * Date: 7/7/2018
- * Time: 12:34 AM
- */
+
 if (!(isset($_SESSION['uid']) && isset($_SESSION['type']))) {
     if($_SESSION['type']!='parent'){}
     header("Location: ../Login/index.html?error=login");
