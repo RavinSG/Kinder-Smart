d8c036f471d23d79f677244ab6b99656cd59a152
Harith Sankalpa <harith.sankalpa@gmail.com>
2018-07-07 12:57:49 +0530

    working on teacher add daily details

--- Teacher/update-daily-details-front.php
+++ Teacher/update-daily-details-front.php
@@ -12,9 +12,23 @@ require_once ("checklogin.teacher.php")?>
     <title>Update Daily Details</title>
 </head>
 <body>
+<div class="navbar">
+    <a class="active" href="home.html">Home</a>
+    <a href="applyLeave.php">Apply Leave</a>
+    <a href="markAttendance.php">Mark Attendance</a>
+    <a href="viewSyllabus.php">Syllabus</a>
+    <a href="viewChildRemoveRequests.php">Child Remove Requests</a>
+    <a href="sentMessage.php">Send Message</a>
+    <a href="#">Settings</a>
+    <a href="#">Logout</a>
+</div>
     <h2>Daily Details</h2>
     <form action="update-daily-details.php" method="POST">
-        <textarea name=""></textarea>
+        <label for="homework">Homework : </label>
+        <textarea name="homework" placeholder="Enter Homework For the Day Here"></textarea>
+        <label for="spec_note">Special note : </label>
+        <textarea name="spec_note" placeholder="Enter Special Notes For the Day Here"></textarea>
+        <input type="submit" value="submit">
     </form>
 </body>
 </html>
