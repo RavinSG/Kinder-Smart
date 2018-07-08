68b6ad6a473e095c60fe0ff3106a601f1c9095ce
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-07-08 15:08:21 +0530

    Resolved connection error in add child form.

--- include/conToChildren.php
+++ include/conToChildren.php
@@ -10,23 +10,23 @@ if (isset($_POST['submit'])){
     $parent = $_POST['parent'];
 
     if (empty($first) or empty($last) or empty($age) or empty($contact) or empty($parent)){
-        header("Location: ../Admin/registration/addChild.php?add=empty&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
+        header("Location: ../Admin/addChild.php?add=empty&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
         return;
 
     } elseif ($age<=0){
-        header("Location: ../Admin/registration/addChild.php?add=ageError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
+        header("Location: ../Admin/addChild.php?add=ageError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
         return;
 
     } elseif ($age>10){
-        header("Location: ../Admin/registration/addChild.php?add=old&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
+        header("Location: ../Admin/addChild.php?add=old&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
         return;
 
     } elseif ((1 === preg_match('~[0-9]~', $first)) or (1 === preg_match('~[0-9]~', $last))){
-        header("Location: ../Admin/registration/addChild.php?add=nameError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
+        header("Location: ../Admin/addChild.php?add=nameError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
         return;
 
     } elseif (preg_match('~[a-z]~', $contact)){
-        header("Location: ../Admin/registration/addChild.php?add=contactError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
+        header("Location: ../Admin/addChild.php?add=contactError&first=$first&last=$last&age=$age&contact=$contact&parent=$parent");
         return;
 
     }
@@ -45,12 +45,12 @@ VALUES (:fname, :lname, :age, :contact, :parent)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
 
-        header("Location: ../Admin/registration/addChild.php?add=successful");
+        header("Location: ../Admin/addChild.php?add=successful");
         return;
 
 
     }
 
 } else{
-    header('Location: ../Admin/registration/addChild.php?add=error');
+    header('Location: ../Admin/addChild.php?add=error');
 }
