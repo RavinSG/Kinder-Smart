8531ba6e4e01525a92f0d3ad024858c0ce9c46f5
Ravin Gunawardena <RavinSG@users.noreply.github.com>
2018-07-08 17:55:49 +0530

    Resolved teacher login error.

--- Parent/checklogin.parent.php
+++ Parent/checklogin.parent.php
@@ -3,7 +3,6 @@ require_once "../classes/KinderParent.php";
 session_start();
 
 if (isset($_SESSION['uid']) && isset($_SESSION['type'])){
-
     header("Location: ../Login");
 } elseif ($_SESSION['type'] != 'parent'){
     header("Location: ../Login");
--- Teacher/checklogin.teacher.php
+++ Teacher/checklogin.teacher.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Harith
- * Date: 7/7/2018
- * Time: 1:42 AM
- */
 if (!isset($_SESSION['uid'])){
     header("Location: ../Login");
-} elseif ($_SESSION['type'] != 'teacher '){
+} elseif ($_SESSION['type'] != 'teacher'){
     header("Location: ../Login");
 }
 ?>
